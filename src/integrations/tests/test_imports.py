68f103ba8b49fc8479ffb8bce2990e8272b15e51
CrazyTim71 <118295691+CrazyTim71@users.noreply.github.com>
2025-08-26 16:17:20 +0200

    fix tests

--- src/integrations/tests/test_imports.py
+++ src/integrations/tests/test_imports.py
@@ -118,21 +118,15 @@ class ImportAniList(TestCase):
         """Create user for the tests."""
         self.credentials = {"username": "test", "password": "12345"}
         self.user = get_user_model().objects.create_user(**self.credentials)
-        self.importer = anilist.AniListImporter(
-            helpers.encrypt("token"),
-            self.user,
-            "new",
-            "username",
-        )
 
     @patch("requests.Session.post")
-    def test_import_anilist(self, mock_request):
+    def test_import_anilist_public(self, mock_request):
         """Basic test importing anime and manga from AniList."""
         with Path(mock_path / "import_anilist.json").open() as file:
             anilist_response = json.load(file)
         mock_request.return_value.json.return_value = anilist_response
 
-        self.importer.import_data()
+        anilist.importer(None, self.user, "new", "bloodthirstiness")
 
         self.assertEqual(Anime.objects.filter(user=self.user).count(), 4)
         self.assertEqual(Manga.objects.filter(user=self.user).count(), 3)
@@ -151,6 +145,49 @@ class ImportAniList(TestCase):
             .history.first()
             .history_date,
             datetime(2025, 6, 4, 10, 11, 17, tzinfo=UTC),
+        )
+
+    @patch("requests.Session.post")
+    def test_import_anilist_private(self, mock_request):
+        """Basic test importing anime and manga from AniList."""
+        with Path(mock_path / "import_anilist.json").open() as file:
+            anilist_response = json.load(file)
+        mock_request.return_value.json.return_value = anilist_response
+
+        anilist.importer(
+            helpers.encrypt("token"),
+            self.user,
+            "new",
+            "username",
+        )
+
+        self.assertEqual(Anime.objects.filter(user=self.user).count(), 4)
+        self.assertEqual(Manga.objects.filter(user=self.user).count(), 3)
+        self.assertEqual(
+            Anime.objects.get(user=self.user, item__title="FLCL").status,
+            Status.PAUSED.value,
+        )
+        self.assertEqual(
+            Manga.objects.filter(user=self.user, item__title="One Punch-Man")
+            .first()
+            .score,
+            9,
+        )
+        self.assertEqual(
+            Anime.objects.get(user=self.user, item__title="FLCL")
+            .history.first()
+            .history_date,
+            datetime(2025, 6, 4, 10, 11, 17, tzinfo=UTC),
+        )
+
+    def test_user_not_found(self):
+        """Test that an error is raised if the user is not found."""
+        self.assertRaises(
+            helpers.MediaImportError,
+            anilist.importer,
+            "fhdsufdsu",
+            self.user,
+            "new",
         )
 
 

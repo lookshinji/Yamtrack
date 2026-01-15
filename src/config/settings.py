747afc84ec3b39a5b603ef8ff3e89920742df6d3
FuzzyGrim <stromed_chariotman@aleeas.com>
2026-01-15 22:14:09 +0100

    add external links (imdb, tvdb, wikidata) to media details page #937

--- src/config/settings.py
+++ src/config/settings.py
@@ -228,7 +228,7 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_URL,
         "TIMEOUT": CACHE_TIMEOUT,
-        "VERSION": 11,
+        "VERSION": 13,
         "KEY_PREFIX": KEY_PREFIX,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",

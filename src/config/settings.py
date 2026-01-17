12f6933c691f053c2e98cbcbf6946c91d2fb9823
FuzzyGrim <stromed_chariotman@aleeas.com>
2026-01-17 16:33:19 +0100

    prefer edition release date over book release date for hardcover #966

--- src/config/settings.py
+++ src/config/settings.py
@@ -228,7 +228,7 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": REDIS_URL,
         "TIMEOUT": CACHE_TIMEOUT,
-        "VERSION": 13,
+        "VERSION": 14,
         "KEY_PREFIX": KEY_PREFIX,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
